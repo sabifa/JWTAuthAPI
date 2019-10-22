25ed3d0e64fb8ff2e9d51070324e949b568c8662
Fabian Klöckner <fabian.kloeckner@brickmakers.de>
2019-10-22 08:53:41 +0200

    Added launchUrl and changed testController to allow anonymous calls

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,13 +6,13 @@ namespace JWTAuthAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AllowAnonymous]
     public class TestController : ControllerBase
     {
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new { value = "successfull" });
+            return Ok(new { value = "API started" });
         }
     }
 }
